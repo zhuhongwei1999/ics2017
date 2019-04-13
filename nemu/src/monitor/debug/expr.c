2fbc05455c1abc4d0ad86c038879966bfbbdb606
tracer-ics2017 <tracer@njuics.org>
2019-04-14 00:02:29 +0800

    >  compile 161720324 zhuhongwei Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  00:02:29 up 5 min,  2 users,  load average: 0.02, 0.03, 0.00 58aee2c2b87ef2f2b6dcc51cf79c98407575d2f3

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -7,7 +7,11 @@
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ
+  TK_NOTYPE = 256, TK_BIG = 255, TK_SMALL = 254, TK_BIG_EQ = 253, 
+  TK_SMALL_EQ = 252, TK_LS = 251, TK_RS = 250, TK_PLUS = 249, TK_SUB = 248, 
+  TK_MUL = 247, TK_DIV = 246, TK_EQ = 245, TK_DEC = 244, TK_HEX = 243, TK_POINT = 242, 
+  TK_MOD = 241, TK_OR = 240, TK_AND = 239, TK_NOT = 238, TK_$ = 237, TK_NOT_EQ = 236, 
+  TK_NEG = 235, TK_LBRACE = 234, TK_RBRACE = 233, TK_REG = 232
 
   /* TODO: Add more token types */
 
@@ -23,11 +27,33 @@ static struct rule {
    */
 
   {" +", TK_NOTYPE},    // spaces
-  {"\\+", '+'},         // plus
-  {"==", TK_EQ}         // equal
+  {"\\+", TK_PLUS},         // plus
+  {"==", TK_EQ},         // equal
+	{">", TK_BIG},
+	{"<", TK_SMALL},
+	{">=", TK_BIG_EQ},
+	{"<=", TK_SMALL_EQ},
+	{"<<", TK_LS},
+	{">>", TK_RS},
+	{"-", TK_SUB},
+	{"\\*", TK_MUL},
+	{"\\/", TK_DIV},
+	{"[0-9]{1,10}", TK_DEC},
+	{"[0-9a-fA-F]{1,8}", TK_HEX},
+	{"\\.", TK_POINT},
+	{"\\%", TK_MOD},
+	{"&&", TK_AND},
+	{"\\|\\|", TK_OR},
+	{"!", TK_NOT},
+	{"$", TK_$},
+	{"!=", TK_NOT_EQ},
+	{"\\(", TK_LBRACE},
+	{"\\)", TK_RBRACE},
+	{"[a-z]{1,5}", TK_REG},
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
+//rules的个数
 
 static regex_t re[NR_REGEX];
 
@@ -78,11 +104,69 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-
         switch (rules[i].token_type) {
-          default: TODO();
+					case TK_NOTYPE:
+            break;
+          case TK_LBRACE:
+            tokens[nr_token++].type = TK_LBRACE;
+            break;
+          case TK_RBRACE:
+            tokens[nr_token++].type = TK_RBRACE;
+            break;
+          case TK_$:
+            tokens[nr_token++].type = TK_$;
+            break;
+          case TK_MUL:
+            tokens[nr_token++].type = TK_MUL;
+            break;
+          case TK_DIV:
+            tokens[nr_token++].type = TK_DIV;
+            break;
+          case TK_PLUS:
+            tokens[nr_token++].type = TK_PLUS;
+            break;
+          case TK_SUB:
+            tokens[nr_token++].type = TK_SUB;
+            break;
+          case TK_MOD:
+            tokens[nr_token++].type = TK_MOD;
+            break;
+          case TK_LS:
+            tokens[nr_token++].type = TK_LS;
+            break;
+					case TK_RS:
+            tokens[nr_token++].type = TK_RS;
+            break;
+          case TK_BIG:
+            tokens[nr_token++].type = TK_BIG;
+            break;
+          case TK_SMALL:
+            tokens[nr_token++].type = TK_SMALL;
+            break;
+          case TK_BIG_EQ:
+            tokens[nr_token++].type = TK_BIG_EQ;
+            break;
+          case TK_SMALL_EQ:
+            tokens[nr_token++].type = TK_SMALL_EQ;
+            break;
+          case TK_NOT_EQ:
+            tokens[nr_token++].type = TK_NOT_EQ;
+            break;
+          case TK_EQ:
+            tokens[nr_token++].type = TK_EQ;
+            break;
+          case TK_DEC:
+            tokens[nr_token++].type = TK_DEC;
+            break;
+          case TK_HEX:
+            tokens[nr_token++].type = TK_HEX;
+            break;
+          case TK_REG:
+            tokens[nr_token++].type = TK_REG;
+            break;
+          default:
+            TODO();
         }
-
         break;
       }
     }
@@ -94,6 +178,183 @@ static bool make_token(char *e) {
   }
 
   return true;
+}
+
+bool check_parentheses(int p, int q){
+  if(tokens[p].type == TK_LBRACE && tokens[q].type == TK_RBRACE){
+    int cnt = 0;
+    for(int i=p+1; i<q; i++){
+      if(tokens[i].type == TK_LBRACE) cnt++;
+      else if(tokens[i].type == TK_RBRACE) cnt--;
+      if(cnt < 0) return false;
+    }
+    if(cnt == 0) return true;
+		else return false;
+  }
+  else return false;
+}
+
+int domiant_operator(int p, int q){
+  int op = -1, cnt = 0, op_type = -1;
+  for(int i=p; i<=q; i++){
+    if(tokens[i].type == TK_LBRACE) cnt++;
+    else if(tokens[i].type == TK_RBRACE) cnt--;
+    else if(cnt!=0 || tokens[i].type==TK_HEX || tokens[i].type==TK_DEC) continue;
+    else if(cnt == 0){
+      if(tokens[i].type==TK_MUL || tokens[i].type==TK_DIV || tokens[i].type==TK_MOD){
+        if(op_type != TK_PLUS && op_type != TK_SUB && op_type != TK_LS && op_type != TK_RS &&
+         op_type != TK_BIG_EQ && op_type != TK_SMALL_EQ && op_type != TK_SMALL && op_type != TK_BIG && 
+         op_type != TK_NOT_EQ && op_type != TK_EQ && op_type != TK_AND && op_type != TK_OR){
+           op_type = tokens[i].type;
+           op = i;
+         }
+      }
+
+      if(tokens[i].type==TK_PLUS || tokens[i].type==TK_SUB){
+        if(op_type != TK_LS && op_type != TK_RS &&
+         op_type != TK_BIG_EQ && op_type != TK_SMALL_EQ && op_type != TK_SMALL && op_type != TK_BIG && 
+         op_type != TK_NOT_EQ && op_type != TK_EQ && op_type != TK_AND && op_type != TK_OR){
+          op_type = tokens[i].type;
+          op = i;
+        }
+      }
+
+      if(tokens[i].type==TK_LS || tokens[i].type==TK_RS){
+        if(op_type != TK_BIG_EQ && op_type != TK_SMALL_EQ && op_type != TK_SMALL && op_type != TK_BIG && 
+          op_type != TK_NOT_EQ && op_type != TK_EQ && op_type != TK_AND && op_type != TK_OR){
+          op_type = tokens[i].type;
+          op = i;
+        }
+      }
+
+      if(tokens[i].type==TK_BIG_EQ || tokens[i].type==TK_SMALL_EQ || tokens[i].type==TK_BIG || tokens[i].type==TK_SMALL){
+        if(op_type != TK_NOT_EQ && op_type != TK_EQ && op_type != TK_AND && op_type != TK_OR){
+          op_type = tokens[i].type;
+          op = i;
+        }
+      }
+
+      if(tokens[i].type==TK_EQ || tokens[i].type==TK_NOT_EQ){
+        if(op_type != TK_AND && op_type != TK_OR){
+          op_type = tokens[i].type;
+          op = i;
+        }
+      }
+
+      if(tokens[i].type==TK_EQ || tokens[i].type==TK_NOT_EQ){
+        if(op_type != TK_AND && op_type != TK_OR){
+          op_type = tokens[i].type;
+          op = i;
+        }
+      }
+
+      if(tokens[i].type==TK_AND){
+        if(op_type != TK_OR){
+          op_type = tokens[i].type;
+          op = i;
+        }
+      }
+
+      if(tokens[i].type==TK_OR){
+        op_type = tokens[i].type;
+        op = i;
+      }
+    }
+  }
+  return op;
+}
+
+uint32_t eval(int p, int q) {
+    if (p > q) {
+      printf("Bad Expression!\n");
+      assert(0);
+    }
+    else if (p == q) {
+      uint32_t sum;
+      if (tokens[p].type == TK_DEC){
+        sscanf(tokens[p].str, "%d", &sum);
+      }
+      else if (tokens[p].type == TK_HEX){
+        sscanf(tokens[p].str, "%x", &sum);
+      }
+      else{
+        printf("Bad Expression!\n");
+        assert(0);
+      }
+      return sum;
+    }
+        /* Single token.
+        * For now this token should be a number.
+        * Return the value of the number.
+        */
+    else if (check_parentheses(p, q) == true) {
+        /* The expression is surrounded by a matched pair of parentheses.
+        * If that is the case, just throw away the parentheses.
+        */
+        return eval(p + 1, q - 1);
+    }
+    else {
+        /* We should do more things here. */
+        uint32_t op, val_1, val_2, result;
+        op = domiant_operator(p, q);
+        if(op == -1){
+          if(tokens[p].type == TK_NEG){
+            sscanf(tokens[q].str, "%x", &result);
+            result = -result;
+          }
+
+          if(tokens[p].type == TK_$){
+            if (strcmp("eip", tokens[q].str) == 0) result = cpu.eip;
+            for (int i = 0; i < 8; i++)
+              if (strcmp(regsl[i], tokens[q].str) == 0) result = cpu.gpr[i]._32;
+            }
+
+          return result;
+        }
+        val_1 = eval(p, op - 1);
+        val_2 = eval(op + 1, q);
+
+        switch (tokens[op].type){
+          case TK_PLUS:
+            return val_1 + val_2;
+          case TK_SUB:
+            return val_1 - val_2;
+          case TK_MUL:
+            return val_1 * val_2;
+          case TK_DIV:
+            return val_1 / val_2;
+          case TK_MOD:
+            return val_1 % val_2;
+          case TK_LS:
+            return val_1 << val_2;
+          case TK_RS:
+            return val_1 >> val_2;
+          case TK_SMALL_EQ:
+            if (val_1 <= val_2) return 1;
+            else return 0;
+          case TK_BIG_EQ:
+            if (val_1 >= val_2) return 1;
+            else return 0;
+          case TK_SMALL:
+            if (val_1 < val_2) return 1;
+            else return 0;
+          case TK_BIG:
+            if (val_1 > val_2) return 1;
+            else return 0;
+          case TK_EQ:
+            return val_1 == val_2;
+          case TK_NOT_EQ:
+            return val_1 != val_2;
+          case TK_AND:
+            if (val_1 == 1 && val_2 == 1) return 1;
+            else return 0;
+          case TK_OR:
+            if (val_1 == 1 || val_2 == 1) return 1;
+            else return 0;
+          default:
+            assert(0);
+        }
+    }
 }
 
 uint32_t expr(char *e, bool *success) {
