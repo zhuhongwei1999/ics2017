d8bf57d6fc5507d21271d4af5758c1ba60225cba
tracer-ics2017 <tracer@njuics.org>
2019-04-14 19:01:53 +0800

    >  compile 161720324 zhuhongwei Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  19:01:53 up  4:32,  1 user,  load average: 0.00, 0.00, 0.00 2f16653b85693f3045c396d1e325fdf2c8695da3

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -98,8 +98,12 @@ static bool make_token(char *e) {
 
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
-        position += substr_len;
 
+        for (int j = 0; j < 32; j++)
+          tokens[nr_token].str[j] = '\0';
+        //???????????
+        strncpy(tokens[nr_token].str, e + position, substr_len);
+        position += substr_len;
         /* TODO: Now a new token is recognized with rules[i]. Add codes
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
@@ -271,7 +275,7 @@ uint32_t eval(int p, int q) {
     }
     else if (p == q) {
       //?????????????
-      uint32_t sum;
+      uint32_t sum = 0;
       if (tokens[p].type == TK_DEC){
         sscanf(tokens[p].str, "%d", &sum);
       }
