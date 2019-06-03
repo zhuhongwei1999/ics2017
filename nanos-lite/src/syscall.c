061f9fe53088b102a5490e1fcab203535b7916af
tracer-ics2017 <tracer@njuics.org>
2019-06-03 23:10:48 +0800

    >  run 161720324 zhuhongwei Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  23:10:48 up  3:28,  2 users,  load average: 0.00, 0.00, 0.00 cdea670c7293c9adf3b7736f67bd4e06eff8d16a

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,9 +1,25 @@
 #include "common.h"
 #include "syscall.h"
+extern ssize_t fs_write(int fd, const void *buf, size_t len);
+
+uintptr_t sys_write(int fd, const void *buf, size_t len) {
+	int i=0;
+	if (fd==1||fd==2){
+    Log("it's log\n");
+		for(;len>0;len--,i++){
+			_putc(((char*)buf)[i]);
+		}
+	}
+	return i;
+}
 
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
+	a[2] = SYSCALL_ARG3(r);
+	a[3] = SYSCALL_ARG4(r);
+
 
   switch (a[0]) {
     case SYS_none:{
@@ -14,6 +30,14 @@ _RegSet* do_syscall(_RegSet *r) {
       _halt(SYSCALL_ARG2(r));
       return NULL;
     }
+    case SYS_brk:{
+       SYSCALL_ARG1(r) = 0;
+       return NULL;
+    }
+    // case SYS_write:{
+    //   SYSCALL_ARG1(r) = fs_write(a[1], (void *)a[2], a[3]);
+		// 	break;  
+    // }
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
