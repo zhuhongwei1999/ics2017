416f0f4cc6d75f0f49793118dbfa43dca148901b
tracer-ics2017 <tracer@njuics.org>
2019-06-03 20:20:58 +0800

    >  run 161720324 zhuhongwei Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  20:20:58 up 38 min,  2 users,  load average: 0.00, 0.00, 0.00 4ccea3a39aa0d1b9e36e178739630167ae251c75

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,8 +1,9 @@
 #include "common.h"
-
 #define DEFAULT_ENTRY ((void *)0x4000000)
+extern size_t get_ramdisk_size();
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
 
-uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+uintptr_t loader(_Protect *as, const char *filename){
+  ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
   return (uintptr_t)DEFAULT_ENTRY;
 }
