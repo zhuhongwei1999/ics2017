d1d0eaf44acee501d9110014b61f199fca536bee
tracer-ics2017 <tracer@njuics.org>
2019-06-07 16:43:35 +0800

    >  run 161720324 zhuhongwei Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  16:43:35 up  1:01,  2 users,  load average: 0.40, 0.42, 0.18 14dce73f2b1273587c6facd488b4ebd5c90bb20

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,7 +4,7 @@
 uintptr_t sys_write(int fd, const void *buf, size_t len) {
 	int i = 0;
 	if (fd==1 || fd==2){
-    Log("sys_write\n");
+    Log("sys_write");
 		for(; len>0; len--,i++){
 			_putc(((char*)buf)[i]);
 		}
