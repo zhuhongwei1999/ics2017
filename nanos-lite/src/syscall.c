f705d2e14a5e29df898cc9b66189f0225bc66c44
tracer-ics2017 <tracer@njuics.org>
2019-06-07 16:03:23 +0800

    >  run 161720324 zhuhongwei Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  16:03:23 up 21 min,  2 users,  load average: 0.00, 0.00, 0.00 7e86a830b3d3c41d2f43eade26671b8f5217c7b0

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,46 +1,52 @@
 #include "common.h"
 #include "syscall.h"
+
+extern char _end;
+
+extern ssize_t fs_read(int fd, void *buf, size_t len);
 extern ssize_t fs_write(int fd, const void *buf, size_t len);
+extern int fs_open(const char *pathname, int flags, int mode);
+extern off_t fs_lseek(int fd, off_t offset, int whence);
+extern int fs_close(int fd);
+
+extern int mm_brk(uint32_t new_brk);
 
 uintptr_t sys_write(int fd, const void *buf, size_t len) {
-	int i=0;
-	if (fd==1||fd==2){
-    Log("it's log\n");
-		for(;len>0;len--,i++){
+	int i = 0;
+	if (fd==1 || fd==2){
+		for(; len>0; len--,i++){
 			_putc(((char*)buf)[i]);
 		}
 	}
 	return i;
 }
 
+
 _RegSet* do_syscall(_RegSet *r) {
-  uintptr_t a[4];
+  uintptr_t a[4], ret=-1;
   a[0] = SYSCALL_ARG1(r);
   a[1] = SYSCALL_ARG2(r);
 	a[2] = SYSCALL_ARG3(r);
 	a[3] = SYSCALL_ARG4(r);
 
-
   switch (a[0]) {
     case SYS_none:{
-      SYSCALL_ARG1(r) = 1;
-      return NULL;
+      ret=1; 
+      break;
     }
     case SYS_exit:{
-      _halt(SYSCALL_ARG2(r));
-      return NULL;
+      _halt(a[1]);
+      break;
     }
-    case SYS_brk:{
-       SYSCALL_ARG1(r) = 0;
-       return NULL;
+    case SYS_write:{
+      ret = sys_write(a[1], (void *)a[2], a[3]);
+      break;
     }
-    // case SYS_write:{
-    //   SYSCALL_ARG1(r) = fs_write(a[1], (void *)a[2], a[3]);
-		// 	break;  
-    // }
+			
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
+  SYSCALL_ARG1(r)=ret;
+  
   return NULL;
 }
-
