039b62679e38cb06e54d184b9e1b59a2ed6f6b05
tracer-ics2017 <tracer@njuics.org>
2019-06-09 00:58:24 +0800

    >  compile 161720324 zhuhongwei Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  00:58:24 up 30 min,  2 users,  load average: 0.15, 0.17, 0.12 1636b093e4ae5663c0097f33f5a6a3c7617f2c0

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -3,59 +3,40 @@
 /* Condition Code */
 
 void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
-  rtlreg_t temp,temp1,temp2,temp3;
   bool invert = subcode & 0x1;
-  
   enum {
     CC_O, CC_NO, CC_B,  CC_NB,
     CC_E, CC_NE, CC_BE, CC_NBE,
     CC_S, CC_NS, CC_P,  CC_NP,
     CC_L, CC_NL, CC_LE, CC_NLE
   };
-
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:{
-      rtl_get_OF(dest);
+    case CC_O:
+      *dest = cpu.eflags.OF ? 1 : 0;
       break;
-    }  
-    case CC_B:{
-      rtl_get_CF(dest);
+    case CC_B:
+      *dest = cpu.eflags.CF ? 1: 0;
       break;
-    }   
-    case CC_E:{
-      rtl_get_ZF(dest);
-      // printf("cpu.ZF:%d\n",*dest);
+    case CC_E:
+      *dest = cpu.eflags.ZF ? 1 : 0;
       break;
-    }  
-    case CC_BE:{
-      rtl_get_CF(&temp);
-      rtl_get_ZF(&temp1);
-      rtl_or(dest,&temp,&temp1);
+    case CC_BE:
+      *dest = (cpu.eflags.CF || cpu.eflags.ZF) ? 1 : 0;
       break;
-    }  
-    case CC_S:{
-      rtl_get_SF(dest);
+    case CC_S:
+      *dest = cpu.eflags.SF ? 1: 0;
       break;
-    }  
-    case CC_L:{
-      rtl_get_SF(&temp);
-      rtl_get_OF(&temp1);
-      rtl_li(dest,(temp!=temp1));
+    case CC_L:
+      *dest = (cpu.eflags.SF!=cpu.eflags.OF) ? 1 : 0;
       break;
-    }      
     case CC_LE:
-        rtl_get_ZF(&temp);
-        rtl_get_SF(&temp1);
-        rtl_get_OF(&temp2);
-        temp3=(temp1!=temp2?1:0);
-        rtl_or(dest,&temp,&temp3);
-        break;
+      *dest = (cpu.eflags.ZF||(cpu.eflags.SF!=cpu.eflags.OF)) ? 1: 0;
+      break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
-
   if (invert) {
     rtl_xori(dest, dest, 0x1);
   }
--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -6,18 +6,21 @@ make_EHelper(mov) {
 }
 
 make_EHelper(push) {
-  rtl_push(&id_dest->val);  
-  print_asm_template1(push);
+  if (id_dest->width == 1) {
+		id_dest->val = (int32_t)(int8_t)id_dest->val;
+	}
+	rtl_push(&id_dest->val);
+  print_asm_template1(push);//打印显示在屏幕上的汇编代码
 }
 
 make_EHelper(pop) {
-  rtl_pop(&t0);
-  operand_write(id_dest, &t0);
+  rtl_pop(&t0);//t0即pop出来的那个值
+  operand_write(id_dest,&t0);//将该值写入id_dest中
   print_asm_template1(pop);
 }
 
 make_EHelper(pusha) {
-  t0 = cpu.esp;
+  t0=cpu.esp;
   rtl_push(&cpu.eax);
   rtl_push(&cpu.ecx);
   rtl_push(&cpu.edx);
@@ -34,7 +37,7 @@ make_EHelper(popa) {
   rtl_pop(&cpu.edi);
   rtl_pop(&cpu.esi);
   rtl_pop(&cpu.ebp);
-  rtl_pop(&t0);
+  rtl_pop(&t0); // throw esp
   rtl_pop(&cpu.ebx);
   rtl_pop(&cpu.edx);
   rtl_pop(&cpu.ecx);
@@ -46,40 +49,48 @@ make_EHelper(popa) {
 make_EHelper(leave) {
   rtl_mv(&cpu.esp,&cpu.ebp);
   rtl_pop(&cpu.ebp);
+
   print_asm("leave");
 }
 
 make_EHelper(cltd) {
   if (decoding.is_operand_size_16) {
-    rtl_lr_w(&t0, R_AX);
-	  rtl_sext(&t0, &t0, 2);
-	  rtl_sari(&t0, &t0, 16);
-	  rtl_sr_w(R_DX, &t0);
+    rtl_lr(&t0,R_AX,2);
+    rtl_slti(&t0,&t0,0);
+    if(t0){
+      rtl_li(&t1,0xffff);
+      rtl_sr(R_DX,2,&t1);
+    }else{
+      rtl_sr(R_DX,2,&tzero);
+    }
   }
   else {
-    rtl_lr_l(&t0, R_EAX);
-	  rtl_sari(&t0, &t0, 31);
-	  rtl_sari(&t0, &t0, 1);
-	  rtl_sr_l(R_EDX, &t0);
+    rtl_lr(&t0,R_EAX,4);
+    rtl_slti(&t0,&t0,0);
+    if(t0){
+      rtl_li(&t1,0xffffffff);
+      rtl_sr(R_EDX,4,&t1);
+    }else{
+      rtl_sr(R_EDX,4,&tzero);
+    }
   }
-
   print_asm(decoding.is_operand_size_16 ? "cwtl" : "cltd");
 }
 
 make_EHelper(cwtl) {
-  if (decoding.is_operand_size_16) {
-    rtl_lr_b(&t0, R_AX);
-	  rtl_sext(&t0, &t0, 1);
-	  rtl_sr_w(R_AX, &t0);
-  }
-  else {
-    rtl_lr_w(&t0, R_AX);
-    rtl_sext(&t0, &t0, 2);
-	  rtl_sr_l(R_EAX, &t0);
+  if(decoding.is_operand_size_16) {
+    rtl_lr(&t1,R_AL,1);
+    rtl_sext(&t2,&t1,1);
+    rtl_sr(R_AX,2,&t2);
+  }else{
+    rtl_lr(&t1,R_AX,2);
+    rtl_sext(&t2,&t1,2);
+    rtl_sr(R_EAX,4,&t2);
   }
 
   print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
 }
+
 make_EHelper(movsx) {
   id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
   rtl_sext(&t2, &id_src->val, id_src->width);
