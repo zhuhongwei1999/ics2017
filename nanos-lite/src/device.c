f144042c82432dee725282fcb8931f3588ff9650
tracer-ics2017 <tracer@njuics.org>
2019-06-09 15:33:42 +0800

    >  run 161720324 zhuhongwei Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  15:33:42 up  4:14,  2 users,  load average: 0.07, 0.02, 0.00 c9f6f2a6878ce20a6b63a51c70094eb6718b6185

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,14 +15,17 @@ size_t events_read(void *buf, size_t len) {
 static char dispinfo[128] __attribute__((used));
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
+  strncpy(buf, dispinfo + offset, len);
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
+  int width= _screen.width;
+  _draw_rect((uint32_t *)(buf), (offset/4)%width, (offset/4)/width, len/4, 1);
 }
 
 void init_device() {
   _ioe_init();
-
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
+  strcpy(dispinfo ,"WIDTH:400\nHEIGHT:300");
 }
