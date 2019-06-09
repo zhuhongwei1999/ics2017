9747f5de137bb97e72ecc5f2918d2c04345eb458
tracer-ics2017 <tracer@njuics.org>
2019-06-09 15:50:30 +0800

    >  run 161720324 zhuhongwei Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  15:50:30 up  4:31,  2 users,  load average: 1.48, 1.37, 0.84 bef740f26fe72e257dc89d40a12ffa611c976840

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -9,23 +9,33 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t len) {
-  return 0;
+  int key = _read_key();
+  char keydown_char = (key & 0x8000 ? 'd' : 'u');
+  key &= ~0x8000;
+  if (key != _KEY_NONE) {
+    return snprintf(buf, len, "k%c %s\n", keydown_char, keyname[key]) - 1;
+  }
+  else {
+    unsigned long time_ms = _uptime();
+    return snprintf(buf, len, "t %d\n", time_ms) - 1;
+  }
 }
 
 static char dispinfo[128] __attribute__((used));
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
-  strncpy(buf, dispinfo + offset, len);
+  memcpy(buf, dispinfo + offset, len);
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
-  int width= _screen.width;
-  _draw_rect((uint32_t *)(buf), (offset/4)%width, (offset/4)/width, len/4, 1);
+  offset /= sizeof(uint32_t);
+  _draw_rect(buf, offset % _screen.width, offset / _screen.width, len / sizeof(uint32_t), 1);
 }
 
 void init_device() {
   _ioe_init();
+
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
-  strcpy(dispinfo ,"WIDTH:400\nHEIGHT:300");
+  sprintf(dispinfo, "WIDTH: %d\nHEIGHT: %d", _screen.width, _screen.height);
 }
