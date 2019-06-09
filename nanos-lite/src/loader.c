b2877315372561e88cfd0cb1ecfe8fac7f7b3e1e
tracer-ics2017 <tracer@njuics.org>
2019-06-09 12:18:26 +0800

    >  run 161720324 zhuhongwei Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  12:18:26 up  3:17,  2 users,  load average: 0.03, 0.03, 0.00 ee41a6d875ea9f7b91a8bf871eb992391cc1e66d

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,21 +1,10 @@
 #include "common.h"
-
 #define DEFAULT_ENTRY ((void *)0x4000000)
-
-extern void ramdisk_read(void *buf, off_t offset, size_t len);
 extern size_t get_ramdisk_size();
-extern ssize_t fs_read(int fd, void *buf, size_t len);
-extern size_t fs_filesz(int fd);
-extern int fs_open(const char *pathname, int flags, int mode);
-extern int fs_close(int fd);
-
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
 uintptr_t loader(_Protect *as, const char *filename) {
-  //ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
-
-	int fd = fs_open(filename, 0, 0);
-	printf("fd = %d\n", fd);
-	fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd)); 
-	fs_close(fd); 
-	
-	return (uintptr_t)DEFAULT_ENTRY;
+//将ramdisk 中从0 开始的所有内容放置在0x4000000,并
+//把这个地址作为程序的入口返回即可.
+ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size());
+return (uintptr_t)DEFAULT_ENTRY;
 }
