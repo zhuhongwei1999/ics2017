92c67a031ca713514c5e4536c82742a05c090d79
tracer-ics2017 <tracer@njuics.org>
2019-06-16 11:04:44 +0800

    >  compile 161720324 zhuhongwei Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  11:04:44 up  1:38,  2 users,  load average: 0.97, 0.61, 0.25 bd6ddb021c4e0dba8465512a1abd3446282c51bb

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -19,20 +19,19 @@ void free_page(void *p) {
 int mm_brk(uint32_t new_brk) {
   if (current->cur_brk == 0) {
     current->cur_brk = current->max_brk = new_brk;
-  }
-  else {
+  } else {
     if (new_brk > current->max_brk) {
-      uintptr_t page_start = PGROUNDUP(current->max_brk);
-      uintptr_t page_end = PGROUNDUP(new_brk);
-      for (; page_start <= page_end; page_start += PGSIZE) {
-        _map(&current->as, (void *)page_start, new_page());
+      // TODO: map memory region [current->max_brk, new_brk)
+      // into address space current->as
+      uintptr_t pa, va;
+      for (va = (current->max_brk+0xfff) & ~0xfff; va < new_brk; va += PGSIZE) {
+        pa = (uintptr_t)new_page();
+        _map(&current->as, (void *)va, (void *)pa);
       }
-      current->max_brk = new_brk;
-    }
+      current->max_brk = va;
+    } 
     current->cur_brk = new_brk;
   }
-
-  // success
   return 0;
 }
 
